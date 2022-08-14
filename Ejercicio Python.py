e7b262914776b46c4bb702b34507c3bc3469d035
Franbelt1 <108037184+Franbelt1@users.noreply.github.com>
2022-08-14 13:43:21 -0300

    Add files via upload

--- Ejercicio Python.py
+++ Ejercicio Python.py
@@ -1,15 +1,19 @@
-# 5) Crear una tupla en Python con el nombre de “Historial” la cual contenga los siguientes valores:
-# 2350, 5960, 23000, 1000, 8900
-# Que representa montos de atención en pesos por diferentes servicios/consultas de la mascota “Puppy”.
-# Calcular el monto total gastado a lo largo del tiempo por atención de “Puppy”. Si el gasto efectuado
-# es menor a 30000, mostrar en pantalla dicho resultado, caso contrario mostrar el mensaje “Gastos por
-# encima de lo presupuestado”.
 
-Historial = (2350, 5960, 23000, 1000, 8900)
-total = 0
-for i in range(len(Historial)):
-    total = total + Historial[i]
-if total < 30000:
-    print('Gasto total:',total)
-else:
-    print('Gastos por encima de lo presupuestado')
+def valor_minimo(valores):
+    """
+    Calcular el valor mínimo de atención gastada en “Olivia”.
+    """
+    
+    minimo = valores[0]
+
+    for i in range(1, len(valores)):
+        if valores[i] < minimo:
+            minimo = valores [i]
+
+    return minimo
+
+Historial4 =[ 7510, 7960, 76180, 800, 4100]
+
+print(valor_minimo(Historial4))
+
+                    
